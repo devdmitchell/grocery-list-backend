07218720314d599d81989713f2149f009c9d86cd
devdmitchell <dmitchell.prsvr@gmail.com>
2025-02-11 09:26:25 -0600

    completed app.js file

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express')
     const logger = require('morgan')
     const mongoose = require('mongoose')
     const cors = require('cors')
-    
+    const grocerListRouter = require('./routes/GroceryList/groceryListRouter')
 
     const app = express()
 
     app.use(cors())
     app.use(logger('dev'))
     app.use(express.json())
+    app.use('/api/grocery', grocerListRouter)
 
 
 
