16766f700478994cfab910c9b97b592ab53afae2
devdmitchell <dmitchell.prsvr@gmail.com>
2025-02-12 08:06:59 -0600

    updated backend files

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const express = require('express')
     app.use(cors())
     app.use(logger('dev'))
     app.use(express.json())
-    app.use('/api/grocery', grocerListRouter)
+    app.use('/api/grocery', groceryListRouter)
 
 
 
