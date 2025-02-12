a5ae26dadc8e14b27f41d62c30312409d2c27688
devdmitchell <dmitchell.prsvr@gmail.com>
2025-02-12 08:04:47 -0600

    completed grocery router

--- routes/GroceryList/groceryListRouter.js
+++ routes/GroceryList/groceryListRouter.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const groceryController = require('./controller/groceryController')
 
 
-
-
+router.get('/get-all-groceries', groceryController.getAllGroceries)
+router.post('/create-grocery', groceryController.createGrocery)
+router.put('/update-grocery-by-id/:id', groceryController.updateGroceryById)
+router.delete('/delete-grocery-by-id/:id', groceryController.deleteGroceryById)
 
 
 module.exports = router
